8c9fcc503e6b06a162f6bd33e4f8403d8325f625
Ondrej Chaloupka <ochaloup@redhat.com>
2012-06-26 11:36:40 +0200

    Test on remote ejb client connected with ssl

---
+++ testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/ejb/remote/ssl/SSLEJBRemoteClientTestCase.java
@@ -0,0 +1,111 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2011, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.jboss.as.test.integration.ejb.remote.ssl;
+
+import java.io.File;
+import java.util.Properties;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.container.test.api.RunAsClient;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.test.api.ArquillianResource;
+import org.jboss.as.arquillian.api.ServerSetup;
+import org.jboss.as.arquillian.container.ManagementClient;
+import org.jboss.as.test.integration.ejb.remote.client.selector.EJBClientContextSelectorChangingBean;
+import org.jboss.as.test.integration.ejb.util.EJBClientContextSelector;
+import org.jboss.ejb.client.ContextSelector;
+import org.jboss.ejb.client.EJBClientContext;
+import org.jboss.logging.Logger;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.exporter.ZipExporter;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * Testing ssl connection of ssl client.
+ * @author Ondrej Chaloupka
+ */
+@RunWith(Arquillian.class)
+@ServerSetup(SSLRealmSetupTask.class)
+@RunAsClient
+public class SSLEJBRemoteClientTestCase {
+    private static final Logger log = Logger.getLogger(SSLEJBRemoteClientTestCase.class);
+    private static final String MODULE_NAME = "ssl-remote-ejb-client-test";
+
+    @Deployment
+    public static Archive<?> deploy() {
+        final JavaArchive jar = ShrinkWrap.create(JavaArchive.class, MODULE_NAME + ".jar");
+        jar.addClasses(StatelessBeanRemote.class, StatelessBean.class);        
+        File testPackage = new File("/tmp", "ssl-remote-ejb-client-test.jar");
+        jar.as(ZipExporter.class).exportTo(testPackage, true);
+        return jar;
+    }
+    
+    @Test
+    public void test(@ArquillianResource ManagementClient managementClient) throws Exception {       
+        // Defining remoting connector to use ssl security realm
+        SSLRealmSetupTask.setRemotingConnectorRealm(managementClient, SSLRealmSetupTask.SECURITY_REALM_NAME);
+        SSLRealmSetupTask.reload(managementClient);
+        ContextSelector<EJBClientContext> previousSelector = null;
+
+        try {
+            // Defining for client where to find certificate
+            System.setProperty("javax.net.ssl.trustStore", SSLRealmSetupTask.KEYSTORE_ABSOLUTE_PATH + SSLRealmSetupTask.KEYSTORE_CLIENT_FILENAME);
+            System.setProperty("javax.net.ssl.trustStorePassword", SSLRealmSetupTask.KEYSTORE_PASSWORD);
+            // System.setProperty("javax.net.ssl.keyStore", SSLRealmSetupTask.KEYSTORE_CLIENT_FILENAME);
+         
+            // Taken properties defined to use SSL
+            // previousSelector = EJBClientContextSelector.setup(SSLRealmSetupTask.KEYSTORE_RELATIVE_PATH + File.separator + "jboss-ejb-client.properties");
+            
+            Properties env = new Properties();
+            env.setProperty(Context.URL_PKG_PREFIXES, "org.jboss.ejb.client.naming");
+            
+            InitialContext ctx = new InitialContext(env);
+            
+            StatelessBeanRemote bean = (StatelessBeanRemote) ctx.lookup(String.format("ejb:/%s//%s!%s", 
+                    MODULE_NAME, StatelessBean.class.getSimpleName(), StatelessBeanRemote.class.getName()));
+            Assert.assertEquals("Remote connection of EJB client through SSL was not successful", StatelessBeanRemote.ANSWER, bean.sayHello());
+        } finally {
+            if(previousSelector != null) {
+                EJBClientContext.setSelector(previousSelector);
+            }
+            // SSLRealmSetupTask.setRemotingConnectorRealm(managementClient, SSLRealmSetupTask.PREVIOUS_SECURITY_REALM_NAME);
+        }
+    }
+    
+    public static void main(String[] args) throws Exception {
+        Properties env = new Properties();
+        env.setProperty(Context.URL_PKG_PREFIXES, "org.jboss.ejb.client.naming");       
+        InitialContext ctx = new InitialContext(env);
+        StatelessBeanRemote bean = (StatelessBeanRemote) ctx.lookup(String.format("ejb:/%s//%s!%s",
+                MODULE_NAME, StatelessBean.class.getSimpleName(), StatelessBeanRemote.class.getName()));
+        System.out.println("Preparing to say hello...");
+        System.out.println(bean.sayHello());
+    }
+}
---
+++ testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/ejb/remote/ssl/SSLRealmSetupTask.java
@@ -0,0 +1,211 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2012, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.jboss.as.test.integration.ejb.remote.ssl;
+
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.*;
+
+import java.io.File;
+import java.net.URL;
+
+import org.jboss.as.arquillian.api.ServerSetupTask;
+import org.jboss.as.arquillian.container.ManagementClient;
+import org.jboss.as.test.integration.jca.JcaMgmtBase;
+import org.jboss.dmr.ModelNode;
+import org.jboss.logging.Logger;
+import org.junit.Assert;
+
+/**
+ * Setup for ssl ejb remote connection.
+ * Keystore created on basis of tutorial at https://community.jboss.org/wiki/SSLSetup.
+ *
+ * @author Ondrej Chaloupka
+ */
+public class SSLRealmSetupTask implements ServerSetupTask {
+    private static final Logger log = Logger.getLogger(SSLRealmSetupTask.class);
+
+    public static String PREVIOUS_SECURITY_REALM_NAME;
+    public static String KEYSTORE_ABSOLUTE_PATH;
+    public static final String SECURITY_REALM_NAME = "SSLRealm";
+    public static final String KEYSTORE_RELATIVE_PATH = "ejb" + File.separator + "keystore";
+    public static final String KEYSTORE_SERVER_FILENAME = "server.keystore";
+    public static final String KEYSTORE_CLIENT_FILENAME = "client.keystore";
+    public static final String KEYSTORE_PASSWORD = "123456";
+    public static final String AUTHENTICATION_PROPERTIES_PATH = "application-users.properties";
+    public static final String AUTHENTICATION_PROPERTIES_RELATIVE_TO = "jboss.server.config.dir";
+    
+
+    public static ModelNode getSecurityRealmsAddress() {
+        ModelNode address = new ModelNode();
+        address.add(CORE_SERVICE, MANAGEMENT);
+        address.add(SECURITY_REALM, SECURITY_REALM_NAME);
+        return address;
+    }
+    
+    public static ModelNode getSecurityRealmsAddressSSLIdentity() {
+        ModelNode address = getSecurityRealmsAddress();
+        address.add(SERVER_IDENTITY, SSL);
+        address.protect();
+        return address;
+    }
+    
+    public static ModelNode getSecurityRealmsAddressAuthentication() {
+        ModelNode address = getSecurityRealmsAddress();
+        address.add(AUTHENTICATION, PROPERTIES);
+        address.protect();
+        return address;
+    }
+    
+    public static ModelNode getRemotingConnectorAddress() {
+        ModelNode address = new ModelNode();
+        address.add(SUBSYSTEM, "remoting");
+        address.add("connector", "remoting-connector");
+        address.protect();
+        return address;
+    }
+    
+    /**
+     * Inspiration from {@link JcaMgmtBase}
+     */
+    public static void reload(final ManagementClient managementClient) throws Exception {
+        ModelNode operation = new ModelNode();
+        operation.get(OP).set("reload");
+        log.info("Executing operation :reload on DMR");
+        managementClient.getControllerClient().execute(operation);
+        boolean reloaded = false;
+        int i = 0;
+        while (!reloaded) {
+            try {
+                Thread.sleep(5000);
+                if (managementClient.isServerInRunningState())
+                    reloaded = true;
+            } catch (Throwable t) {
+                // nothing to do, just waiting
+            } finally {
+                if (!reloaded && i++ > 10)
+                    throw new Exception("Server reloading failed");
+            }
+        }
+    }
+
+    /**
+     * Associating the realm with remoting connector 
+     * /subsystem=remoting/connector=remoting-connector:write-attribute(name=security-realm, value=SSLRealm)
+     */
+    public static void setRemotingConnectorRealm(final ManagementClient managementClient, final String realmName) throws Exception {
+        ModelNode operation = new ModelNode();
+        operation.get(OP_ADDR).set(SSLRealmSetupTask.getRemotingConnectorAddress());
+        operation.get(OP).set(WRITE_ATTRIBUTE_OPERATION);
+        operation.get(NAME).set("security-realm");
+        operation.get(VALUE).set(realmName);
+        ModelNode result = managementClient.getControllerClient().execute(operation);
+        log.infof("Setting security realm %s to remoting connector with result %s", SSLRealmSetupTask.SECURITY_REALM_NAME, result);
+        Assert.assertEquals(SUCCESS, result.get(OUTCOME).asString());
+        SSLRealmSetupTask.reload(managementClient);
+    }
+   
+    
+    /**
+    * <security-realm name="SSLRealm">
+    *    <server-identities>
+    *        <ssl>
+    *            <keystore path="server.keystore" relative-to="[Thread.currentThread()]/[ejb/keystore]" keystore-password="123456"/>
+    *        </ssl>
+    *    </server-identities>
+    *    <authentication>
+    *        <properties path="application-users.properties" relative-to="jboss.server.config.dir"/>
+    *    </authentication>
+    * </security-realm>
+     */
+    @Override
+    public void setup(final ManagementClient managementClient, final String containerId) throws Exception {       
+        // Adding security realm
+        ModelNode secRealmAddress = getSecurityRealmsAddress();
+        secRealmAddress.protect();
+        ModelNode operation = new ModelNode();
+        operation.get(OP_ADDR).set(secRealmAddress);
+        operation.get(OP).set(ADD);
+        ModelNode result = managementClient.getControllerClient().execute(operation);
+        log.infof("Adding security realm %s with result %s", SECURITY_REALM_NAME, result);
+        Assert.assertEquals(SUCCESS, result.get(OUTCOME).asString());
+
+        // Adding ssl attribute
+        // /core-service=management/security-realm=SSLRealmOndra/server-identity=ssl:add(
+        //   keystore-password=123456, keystore-relative-to=jboss.server.config.dir, keystore-path=keystore)
+        ClassLoader tccl = Thread.currentThread().getContextClassLoader();
+        URL resourcesUrl = tccl.getResource("");
+        String resourcePath = resourcesUrl.getPath();
+        log.info("Path to resources is " + resourcePath);
+        // String testAbsPath = new File(SSLEJBRemoteClientTestCase.class.getProtectionDomain().getCodeSource().getLocation().getPath()).getAbsolutePath();
+        operation = new ModelNode();
+        operation.get(OP_ADDR).set(getSecurityRealmsAddressSSLIdentity());
+        operation.get(OP).set(ADD);
+        operation.get("keystore-password").set(KEYSTORE_PASSWORD);
+        KEYSTORE_ABSOLUTE_PATH =  resourcePath + KEYSTORE_RELATIVE_PATH;
+        operation.get("keystore-relative-to").set(KEYSTORE_ABSOLUTE_PATH);
+        operation.get("keystore-path").set(KEYSTORE_SERVER_FILENAME);
+        result = managementClient.getControllerClient().execute(operation);
+        log.infof("Setting server-identity ssl for realm %s (password %s, keystore path %s%s%s) with result %s", SECURITY_REALM_NAME,
+                KEYSTORE_PASSWORD, KEYSTORE_ABSOLUTE_PATH, result);
+        Assert.assertEquals(SUCCESS, result.get(OUTCOME).asString());
+        reload(managementClient);
+        Thread.sleep(10000);
+          
+        operation = new ModelNode();
+        operation.get(OP_ADDR).set(getRemotingConnectorAddress());
+        operation.get(OP).set(READ_ATTRIBUTE_OPERATION);
+        operation.get(NAME).set("security-realm");
+        result = managementClient.getControllerClient().execute(operation);
+        log.info("Reading attribute security-realm with result " + result);
+        Assert.assertEquals(SUCCESS, result.get(OUTCOME).asString());
+        PREVIOUS_SECURITY_REALM_NAME = result.get("result").asString();
+        
+        // Adding authentication attribute
+        // authentication=properties:add(path=application-users.properties, relative-to=jboss.server.config.dir)
+        operation = new ModelNode();
+        operation.get(OP_ADDR).set(getSecurityRealmsAddressAuthentication());
+        operation.get(OP).set(ADD);
+        operation.get(PATH).set(AUTHENTICATION_PROPERTIES_PATH);
+        operation.get(RELATIVE_TO).set(AUTHENTICATION_PROPERTIES_RELATIVE_TO);
+        result = managementClient.getControllerClient().execute(operation);
+        log.infof("Setting authentication of %s (path %s, relative to %s) with result %s", SECURITY_REALM_NAME,
+                AUTHENTICATION_PROPERTIES_PATH, AUTHENTICATION_PROPERTIES_RELATIVE_TO, result);
+        Assert.assertEquals(SUCCESS, result.get(OUTCOME).asString());
+        
+        reload(managementClient);
+    }
+
+    @Override
+    public void tearDown(final ManagementClient managementClient, final String containerId) throws Exception {
+        // Removing security realm
+        /*ModelNode secRealmAddress = getSecurityRealmsAddress();
+        secRealmAddress.protect();
+        ModelNode operation = new ModelNode();
+        operation.get(OP_ADDR).set(secRealmAddress);
+        operation.get(OP).set(REMOVE);
+        ModelNode result = managementClient.getControllerClient().execute(operation);
+        log.infof("Removing security realm %s with result %s", SECURITY_REALM_NAME, result);
+        Assert.assertEquals(SUCCESS, result.get(OUTCOME).asString());
+        
+        reload(managementClient); */
+    }
+}
